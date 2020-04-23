acd1679eebf11c6da53c0aa6060be0daf0f80d21
RG11701-YH <1152040555@qq.com>
2020-04-23 14:22:55 +0800

    yh

---
+++ 状态模式/src/StateDemo/DeadState.java
@@ -0,0 +1,8 @@
+package StateDemo;
+
+public class DeadState extends ThreadState {
+	public DeadState() {
+		state = StateSet.DEAD;
+		System.out.println("Ïß³ÌËÀÍö");
+	}
+}
---
+++ 状态模式/src/StateDemo/ThreadState.java
@@ -0,0 +1,5 @@
+package StateDemo;
+
+public abstract class ThreadState {
+	protected int state; //×´Ì¬Ãû
+}
